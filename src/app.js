4e7af4024f6b76af79a228cae8b787f445bc2121
squxq <franciscotomas0430@gmail.com>
2023-02-18 12:34:07 +0000

    src folder structure | config files | utils files | midlewares | app and index files

---
+++ src/app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const mongoSanitize = require("express-mongo-sanitize");
+const compression = require("compression");
+const cors = require("cors");
+const httpStatus = require("http-status");
+const createLocaleMiddleware = require("express-locale");
+const config = require("./config/config");
+const morgan = require("./config/morgan");
+
+const { authLimiter } = require("./middlewares/rateLimiter");
+const { errorConverter, errorHandler } = require("./middlewares/error");
+const ApiError = require("./utils/ApiError");
+const startPolyglot = require("./utils/startPolyglot");
+
+const app = express();
+if (config.env !== "test") {
+  app.use(morgan.successHandler);
+  app.use(morgan.errorHandler);
+}
+
+// set security HTTP headers
+app.use(helmet());
+
+// parse json request body
+app.use(express.json({ limit: "10kb" }));
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+
+// Get the user's locale, and set a default in case there's none
+app.use(
+  createLocaleMiddleware({
+    priority: ["accept-language", "default"],
+    default: "en_US",
+  })
+);
+
+// Start polyglot and set the language in the req with the phrases to be used
+app.use(startPolyglot);
+
+// sanitize request data
+app.use(xss());
+app.use(mongoSanitize());
+
+// gzip compression
+app.use(compression());
+
+// enable cors
+app.use(cors());
+app.options("*", cors());
+
+app.disable("x-powered-by");
+
+// limit repeated failed requests to auth endpoints
+if (config.env === "production") {
+  app.use("/v1/auth", authLimiter);
+}
+
+// v1 api routes
+// app.use("/api/v1", routes);
+// app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(docs));
+
+// send back a 404 error for any unknown api request
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
+});
+
+// app.use(errorConverter)
+app.use(errorConverter);
+
+// handle errors
+app.use(errorHandler);
+
+/**
+ * Exports Express
+ * @public
+ */
+module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,44 @@
+const app = require("./app");
+const config = require("./config/config");
+const logger = require("./config/logger");
+
+// Express usual app.listen()
+const server = app.listen(config.port, () => {
+  logger.info(`
+      ################################################
+      🚀 Server listening on port: ${config.port} 🚀
+      ################################################
+  `);
+});
+
+const exithandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info("Server closed");
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+// Error found --> Server closes
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exithandler();
+};
+
+// listens for uncaught exceptions and unhandled rejections
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledException", unexpectedErrorHandler);
+
+process.on("SIGTERM", () => {
+  logger.info("SIGTERM received");
+  if (server) {
+    server.close();
+  }
+});
+
+// When a signal handler is added (process.once('SIGTERM'), the handler code is scheduled for the next event loop. So behaviour immediately changes even if you just call process.exit() in the handler.
+
+// Promises, or Javascript generally for that matter, don't have the concept of cancellation built in yet. You could track promises but unless the underlying API's being used specifically address ways to cancel tasks then there's not much to do.
